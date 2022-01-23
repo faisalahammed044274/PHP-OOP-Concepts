a88646d3961c475734c18d915634822515fea559
faisalahammed044274 <setu2014@gmail.com>
2022-01-24 03:29:04 +0600

    Excercise

--- 07-Inheritance/Excercise/Phone/PhoneExample.php
+++ 07-Inheritance/Excercise/Phone/PhoneExample.php
@@ -10,4 +10,56 @@
 
 <?php
 
+class Phone
+{
+    protected $name;
+    protected $touchPhone = false;
+
+    function doesTouch()
+    {
+        if ($this->touchPhone) {
+            echo "It's Touch enabled phone." . PHP_EOL;
+        } else {
+            echo "It's Button Phone." . PHP_EOL;
+        }
+    }
+}
+
+class Samsung extends Phone
+{
+
+    function __construct($name, $touchPhone)
+    {
+        $this->name = $name;
+        $this->touchPhone = $touchPhone;
+    }
+
+    function phoneDetails()
+    {
+        echo " Samsung touched phone" . PHP_EOL;
+    }
+}
+
+class Nokia extends Phone
+{
+
+    function __construct($name, $touchPhone)
+    {
+        $this->name =  $name;
+        $this->touchPhone =  $touchPhone;
+    }
+
+    function phoneDetails()
+    {
+        echo " Nokia Button Phone" . PHP_EOL;
+    }
+}
+
+$samsung1 = new Samsung("Galaxy S3", false);
+$samsung1->phoneDetails();
+$samsung1->doesTouch();
+
+$nokia1 = new Nokia("Nokia 3310", true);
+$nokia1->phoneDetails();
+$nokia1->doesTouch();
 ?>
