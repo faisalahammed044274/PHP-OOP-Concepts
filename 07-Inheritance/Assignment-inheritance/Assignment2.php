cdc2ad1919dca4bce926abd29f265dcafe78fc31
faisalahammed044274 <setu2014@gmail.com>
2022-01-25 05:28:05 +0600

    Inheritance

--- 07-Inheritance/Assignment-inheritance/Assignment2.php
+++ 07-Inheritance/Assignment-inheritance/Assignment2.php
@@ -1,3 +1,21 @@
 <?php
 //Assignment 2: Create Doctor and Specialist Class and Show Overriding Concept
+
+class Doctor{
+    function hello(){
+        echo "Hello, I'm a Doctor.";
+    }
+}
+
+class Specialist extends Doctor{
+    function hello(){
+        echo "Hello, I am an orthopedic surgeon.";
+        parent::hello();
+    }
+}
+
+$doctor1 = new Specialist();
+$doctor1->hello();
+
+
 ?>
