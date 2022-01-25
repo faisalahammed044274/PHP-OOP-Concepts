dd3bd113b85c8d282030162f53907ecec18ce08a
faisalahammed044274 <setu2014@gmail.com>
2022-01-25 22:18:26 +0600

    Final Keyword

--- 07-final Keyword/03.Assignment/Assignments.php
+++ 07-final Keyword/03.Assignment/Assignments.php
@@ -2,11 +2,10 @@
 
 //Create a Bank and User Class and use final Keyword
 
-use Bank as GlobalBank;
 
 final class Bank
 {
-    final function balance()
+     function balance()
     {
         echo "Reserved 100000000000" . PHP_EOL;
     }
@@ -19,10 +18,10 @@ class User extends Bank
         echo "Reserver for the user is 1000000" . PHP_EOL;
     }
 }
+ //Final keyword restricted class and methods both
 
 $bank1 = new Bank();
 $bank1->balance();
-
 
 $user1 = new User();
 $user1->balance();
