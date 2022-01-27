48226f1a5008e08e7eb19a9efbdc5048f1869239
faisalahammed044274 <setu2014@gmail.com>
2022-01-28 00:21:01 +0600

    Interface

---
+++ 08-Interface/1-How to Declare and Implement Interface/Car.php
@@ -0,0 +1,8 @@
+<?php
+
+interface Car{
+
+    public function applyBreak();
+    public function increaseSpeed();
+    public function decreaseSpeed();
+}
--- 08-Interface/1-How to Declare and Implement Interface/interface.php
+++ 08-Interface/1-How to Declare and Implement Interface/interface.php
@@ -1,6 +1,29 @@
 <?php
 /*
     Interface - FRAMEWORK
-    
+    ----------------------------
+    ##01 - you cannot define method in interface. 
+    All the methods in interface has to be just definitions.
 
-*/
+    ##02 - Interface are like framework or structure that every class 
+    must define it when they implement the interface.
+
+    ##03 - Any Class can implement one or many Interfaces.
+
+    ##04 - Class that implement interface has to define  
+    all the methods of interface or else it will give error.
+
+    */
+
+interface Car
+{
+    public function applyBreak();
+    public function increaseSpeed();
+    public function decreaseSpeed();
+}
+
+class SportsCar implements Car
+{
+    //Must have the Car functions defined here
+    
+}
