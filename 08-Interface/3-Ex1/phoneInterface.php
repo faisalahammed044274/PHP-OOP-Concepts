3a8c9bb8e32f164b6ab83fae5f759eb26b56e28c
faisalahammed044274 <setu2014@gmail.com>
2022-01-29 03:22:23 +0600

    Use cases of interface

---
+++ 08-Interface/3-Ex1/phoneInterface.php
@@ -0,0 +1,36 @@
+<?php 
+
+/*
+
+Exrecise 1 : CREATE A PHONE INTERFACE AND IMPLEMENT
+IOS CLASS
+
+1. Define Phone Interface with makeCall with number parameter and
+sendMessage with number and message parameter.
+
+2. Define iOS to implement the phone Interface and Implement the methods.
+
+*/ 
+
+interface Phone{
+
+    public function makeCall($number);
+    public function sendMessage($number, $message);
+}
+
+class iOS implements Phone{
+
+    public function makeCall($number){
+        echo "Making Call to $number." . PHP_EOL;
+    }
+
+    public function sendMessage($number, $message){
+        echo "Sending $message to $number." . PHP_EOL;
+    }
+
+}
+
+$iPhoneXII = new iOS();
+$iPhoneXII->makeCall(121121121);
+$iPhoneXII->sendMessage("121121121", "Learn Interface here");
+
---
+++ 08-Interface/UseCasesOfInterface.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+
+Interface
+
+    ## Interfaces are like guideline or contract.
+
+    ## When you want to control how how a class should be defined
+    then you can define some guideline using interface and ask all
+    the implementation class to follow it.
+
+    ## A server can implement  the code and give a interface to 
+    call that methods with the required parameters.
+
+    ## Think like WSDL or YAML or XSD are like definitions of
+    how something is implemented. XML are like Class.
+    
+    
+*/
