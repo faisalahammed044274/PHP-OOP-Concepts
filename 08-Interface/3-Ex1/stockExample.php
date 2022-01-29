ee291dac93ec533b4f9e77c00464fa1dee317988
faisalahammed044274 <setu2014@gmail.com>
2022-01-30 05:59:47 +0600

    Interface

--- 08-Interface/3-Ex1/stockExample.php
+++ 08-Interface/3-Ex1/stockExample.php
@@ -1,11 +1,28 @@
-<?php 
+<?php
 
 // Create Stock Interface and Implement it in the MyStock Class
 
-interface Stock{
+interface Stock
+{
+    public function StockLimit($limitHas);
 
+    public function  CurrentStock($limitHave);
 }
 
-class MyStock implements Stock{
+class MyStock implements Stock
+{
 
+    public function StockLimit($limitHas)
+    {
+        echo "Previous Stock : " . $limitHas . PHP_EOL;
+    }
+
+    public function CurrentStock($limitHave)
+    {
+        echo "Current Stock : " . $limitHave . PHP_EOL;
+    }
 }
+
+$stockLimit1 = new MyStock();
+$stockLimit1->StockLimit(8787878);
+$stockLimit1->CurrentStock(9789797);
